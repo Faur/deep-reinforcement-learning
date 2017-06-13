e4524c8e35a1e9e63c21a22578349a256e365264
Toke Faurby <Tokeee@gmail.com>
2017-06-13 18:52:42 +0900

    Create Experience_buffer

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 
-
+import numpy as np
 
 
 def clip_rewards():
@@ -35,3 +35,67 @@ class Annealer():
 			return self.end_value
 		self.epsilon = self.initial_value - self.initial_value*step/self.period
 		return self.epsilon
+
+class Experience_buffer():
+	""" Consists of a list of 'experience', each of which is a dict.
+		The keys should be the same for experience, but are not set in advance
+	"""
+    def __init__(self, buffer_capacity=int(1e6)):
+        self.buffer = []
+        self.buffer_capacity = buffer_capacity
+    
+    def __str__(self):
+        """ Simply print the content of the buffer"""
+        tostr = ''
+        for item in self.buffer:
+            tostr += item.__str__()
+            tostr += '\n'
+        return tostr
+    
+    def buffer_size(self):
+        """ Return the current number of experiences in the buffer."""
+        return len(self.buffer)
+    
+    def add(self, experience):
+        """ Add ONE experience to the experience buffer"""
+        self.buffer.append(experience)
+        while len(self.buffer) > self.buffer_capacity:
+            self.buffer.pop(0)
+    
+    def sample(self, size):
+        """ Draw 'size' random samples from the experience buffer, 
+            with replacement"""
+        batch = {'obs':[], 'action':[], 'reward':[],
+                      'next_obs':[], 'done':[]}
+        for _ in range(size):
+            i = np.random.randint(low=0, high=self.buffer_size())
+            example = self.buffer[i]
+            for item in example:
+                batch[item].append(example[item])
+        return batch
+                
+    def test():
+        """ Test the following features:
+                __str__
+                add
+                sample
+        """
+        print('Testing Experience_buffer')
+
+        
+        buffer = Experience_buffer(5)
+        for i in range(8):
+            experience = {'obs':i, 'action':i, 'reward':i, 'next_obs':i, 'done':i}
+            print(i, buffer.buffer_size())
+            buffer.add(experience)
+        
+        print('\nContent of buffer')
+        print(buffer)
+        
+        print('A sample')
+        batch = buffer.sample(5)
+        print(type(batch['obs'])) # should retunr a list
+        print(batch)
+
+
+
