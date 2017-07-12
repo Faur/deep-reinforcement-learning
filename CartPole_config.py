69015fcc7c367313bf20eae8a816a0f260351211
Toke Faurby <Tokeee@gmail.com>
2017-07-12 15:16:23 +0900

    Use CartPole-v1

--- CartPole_config.py
+++ CartPole_config.py
@@ -1,12 +1,18 @@
+env_name = 'CartPole-v0'
+env_max_step = 200
+
 env_name = 'CartPole-v1'
+env_max_step = 500
+
+
 layers = [16]
 gamma = 0.99
+# gamma = 0.95
 
 num_state = 4
 num_action = 2
 
 eps = 1e-10 # Small offset to prevent NAN etc.
-
 
 
 ## Actor-Critic specific
